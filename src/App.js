0b277c40d3465494bc2d0e05931b6cc5b19f6b5f
abdulrahman <abdulrahmanahmad_979@outlook.com>
2024-05-12 18:47:36 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+      <div>
+     <h1> Speciality Coffee</h1>
+     <h2> Ethiopia, Colombia, Honduras, and Kenya</h2>
+     <img src="https://assets.eposnow.com/public/blog-images/coffee-shops-profitable__Resampled.jpg" alt="Description of the image" />
     </div>
+    </body>
+    
   );
 }
 
